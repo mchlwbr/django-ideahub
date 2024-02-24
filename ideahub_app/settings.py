8e447485a20e7c6e43839ba2a5ae8be0d23552a6
Michael Weber <michael@mchlwbr.de>
2024-02-24 12:01:42 +0100

    chore: add `django-htmx`

--- ideahub_app/settings.py
+++ ideahub_app/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_htmx",
     "crispy_forms",
     "crispy_bootstrap5",
     "ideahub",
